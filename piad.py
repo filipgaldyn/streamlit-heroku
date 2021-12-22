2f8503ee0f05fdeb49f9d8be8dcd3542ab5ce8c8
filipgaldyn <filip.galdyn.fg@gmail.com>
2021-12-23 00:14:17 +0100

    cos

--- piad.py
+++ piad.py
@@ -84,7 +84,7 @@ st.dataframe(df_selected_time)
 def filedownload(df):
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
-    href = f'<a href="data:file/csv;base64,{b64}" download="playerstats.csv">Download CSV File</a>'
+    href = f'<a href="data:file/csv;base64,{b64}" download="stats.csv">Download CSV File</a>'
     return href
 
 st.markdown(filedownload(df_selected_team), unsafe_allow_html=True)
